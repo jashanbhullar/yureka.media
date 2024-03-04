9bac14c93a21c7141eed2dabec16623dbcb636fa
jashan <20891087+jashanbhullar@users.noreply.github.com>
2024-03-04 15:56:22 +0530

    - new settings

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,11 +1,14 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     PROJECT_NAME: str = "Yureka Media"
 
-    class Config:
-        case_sensitive = True
+    AUTH0_DOMAIN: str
+    AUTH0_API_AUDIENCE: str
+    AUTH0_ISSUER: str
+    AUTH0_ALGORITHMS: str
 
 
 settings = Settings()
