8269a39922248e65f150b122e7a54d6731b56be5
jashan <20891087+jashanbhullar@users.noreply.github.com>
2024-03-04 17:30:08 +0530

    - minor change

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,12 +1,30 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 from starlette.middleware.cors import CORSMiddleware
 
 from src.api import api
 from src.config import settings
 
-app = FastAPI(
-    title=settings.PROJECT_NAME,
-)
+app = FastAPI()
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title=settings.PROJECT_NAME,
+        version="0.0.1",
+        summary="Yureka Media APIs. Copyright@2024",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://yureka-media.pages.dev/favicon.ico"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
 
 app.add_middleware(
     CORSMiddleware,
